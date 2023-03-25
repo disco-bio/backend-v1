add2c2cb51268995e851d969d4c711b20d9ebf89
The Floating String <laurencel2001@gmail.com>
2023-03-24 23:40:02 -0400

    Add minimal Flask app

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+	return '<a href="/login">Login</a>'
+
+@app.route("/login")
+def login():
+	pass
+
+if __name__ == "__main__":
+	app.run(debug=True, port=5000)
